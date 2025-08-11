5ab742a3a0a1c17bce2b82aa4c1df88e7971368e
Sredha <sredhamohan91@gmail.com>
2025-08-11 12:59:51 +0530

    report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
-    baseUrl:'https://practice.expandtesting.com',
+    baseUrl: 'https://practice.expandtesting.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Register mochawesome reporter plugin
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+      // You can add custom node events here too
     },
   },
 });
